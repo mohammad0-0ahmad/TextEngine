7debb9477122907210891daf6042f34d9f71d7e8
95mohahm <kurtular.vadisi@gmail.com>
2020-03-28 12:39:11 +0100

    Created TextFile.open method by following TDD and wrote comments and javadoc for it.

--- src/TextEngine/Tests/TextFile/Open.java
+++ src/TextEngine/Tests/TextFile/Open.java
@@ -3,14 +3,35 @@ package TextEngine.Tests.TextFile;
 import static TextEngine.TextFile.open;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test class that tests TextFile.open method.
+ */
 class Open {
     /**
      * Test method that check the returned value by passing un exist text file.
      */
     @Test
-    void openUnExistFiles() {
-        assertNull(open("C:\\Users\\95mohahm\\Desktop\\test.txt"));
+    void openUnExistFiles(){
+        try {
+            assertNull(open("C:\\Users\\95mohahm\\Desktop\\test.txt"));
+        }catch (Exception e){}
+
     }
+
+    /**
+     *Test method that check the returned value by passing other format files.
+     */
+    @Test
+    void openOtherTypeFiles(){
+        try {
+            assertNull(open("C:\\Users\\95mohahm\\Desktop\\test.png"));
+            assertNull(open("C:\\Users\\95mohahm\\Desktop\\test.pdf"));
+            assertNull(open("C:\\Users\\95mohahm\\Desktop\\test.text"));
+        }catch (Exception e){}
+    }
+
 }
--- src/TextEngine/TextFile.java
+++ src/TextEngine/TextFile.java
@@ -1,5 +1,9 @@
 package TextEngine;
 
+import java.io.*;
+import java.nio.Buffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class TextFile {
@@ -24,11 +28,23 @@ public class TextFile {
 
     ////////////////////////////////////////////////////
     /*>>> Static methods <<<*/
+
     /**
-     *
+     * Opens a text file.
+     * @param filePath the path of the desired file.
+     * @return TextFile object.
+     * @throws IOException exception may occur during the process of the file opening.
      */
-    public static TextFile open(String filePath){
-        return new TextFile("",null);
+    public static TextFile open(String filePath) throws IOException {
+        File file = new File(filePath);
+        if (!file.exists()) {                                                                            // checks if the file is exist.
+            return null;
+        } else if (!file.getName().substring(file.getName().length() - 4).equals(".txt")) {              // checks file format.
+            return null;
+        }
+        StringBuffer content = new StringBuffer();
+        content.append(new String(Files.readAllBytes(Paths.get(filePath))));                             // read the file and store the content inside content variable.
+        return new TextFile(file.getName(),content);
     }
     ////////////////////////////////////////////////////
     /*>>>> Member methods <<<<*/
@@ -36,36 +52,40 @@ public class TextFile {
     /**
      *
      */
-    public String getName(){
+    public String getName() {
         return null;
     }
 
     /**
      *
      */
-    public void showWords(){ }
-
-    /**
-     *
-     */
-    private void isSorted(){}
-
-    /**
-     *
-     */
-    private void sort(){}
-
-    /**
-     *
-     */
-    private int search(String wordToLookFor){
-        return 0;
+    public void showWords() {
     }
 
     /**
      *
+     */
+    private void isSorted() {
+    }
+
+    /**
+     *
+     */
+    private void sort() {
+    }
+
+    /**
+     *
+     */
+    private int search(String wordToLookFor) {
+        return 0;
+    }
+
+    /**
      * @return
      */
-    private ArrayList<Word> contentSplitter(){return null;}
+    private ArrayList<Word> contentSplitter() {
+        return null;
+    }
 
 }
