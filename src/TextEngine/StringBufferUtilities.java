ab668ec7e920843b03ab0953b33ef05e926a83cb
95mohahm <kurtular.vadisi@gmail.com>
2020-03-29 01:58:43 +0100

    Created StringBufferUtilities.isXBeforeY method with its unit tests,necessary comments and javadoc.

--- src/TextEngine/StringBufferUtilities.java
+++ src/TextEngine/StringBufferUtilities.java
@@ -1,17 +1,26 @@
 package TextEngine;
 
-public interface StringBufferUtilities {
+public interface StringBufferUtilities{
     // The following variables represent its names with another word (" ","\t","\n") and holds the ascii values that are related to them.
     int whiteSpace = 32;
     int breakLine = 10;
     int  tab = 9;
     /**
-     *
-     * @param x
-     * @param y
-     * @return
+     * Compare words to know which word in the arguments will take first place when ordering alphabetically. Ascii order is related in some cases.
+     * @param x the first word that will be used in comparision.
+     * @param y the second word that will be used in comparision.
+     * @return flase if the y is before x "by alphabetic order" OR true if x if before or equals with y.
      */
-    default boolean isXBeforeY(String x, String y){return false;}
+    default boolean isXBeforeY(String x, String y){
+        // to avoid incorrect result when having uppercase letter. " To ignore ascii order"
+        x = x.toLowerCase();
+        y = y.toLowerCase();
+        // if the x is before or x = y.
+        if(x.compareTo(y) <=0){
+            return true;
+        }
+        return false;
+    }
 
     /**
      *
---
+++ src/TextEngine/Tests/StringBufferUtilities/IsXBeforeY.java
@@ -0,0 +1,26 @@
+package TextEngine.Tests.StringBufferUtilities;
+
+import TextEngine.StringBufferUtilities;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class IsXBeforeY implements StringBufferUtilities {
+    /**
+     * a test That checks some possible input values.
+     */
+    @Test
+    public void isItComparingCorrectly(){
+        assertTrue(isXBeforeY("aba", "abb"));
+        assertFalse(isXBeforeY("abs", "abb"));
+        assertTrue(isXBeforeY("abs", "absa"));
+        assertTrue(isXBeforeY("Abs", "abs"));
+        assertTrue(isXBeforeY("Abs", "abs"));
+        assertTrue(isXBeforeY("abs", "Abs"));
+        assertFalse(isXBeforeY("Äbs", "zbs"));
+        assertFalse(isXBeforeY("Ah", "aa"));
+        assertFalse(isXBeforeY("Ah", "aa!"));
+        assertFalse(isXBeforeY("1", "00001"));
+        assertFalse(isXBeforeY("1", " 0"));
+        assertFalse(isXBeforeY("1", "-1"));
+    }
+}
